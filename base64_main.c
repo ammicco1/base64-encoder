6ce7575cd236f5bf8c5df3b719dee5057c0311eb
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-01-05 16:08:21 +0100

    main

--- base64_main.c
+++ base64_main.c
@@ -4,8 +4,8 @@
 
 int main(){
     char *base64, *decoded;
-    char str[__MAX_LEN__], str64[__MAX_64_LEN__];
-    int choise;
+    char str[__MAX_LEN__], tmp[__MAX_LEN__], tmp64[__MAX_64_LEN__], str64[__MAX_64_LEN__];
+    int choise, i;
 
     while(1){
         printf("\n---------------------------------------------\n");
@@ -17,9 +17,9 @@ int main(){
         scanf(" %d", &choise); fflush(stdin);
 
         switch(choise){
-            case 1: printf("\nINSERT THE STRING TO ENCODE: "); fgets(str, __MAX_LEN__, stdin); 
+            case 1: printf("\nINSERT THE STRING TO ENCODE: "); fgets(tmp, __MAX_LEN__, stdin); for(i = 0; i < str_len(tmp) - 1; i++){str[i] = tmp[i];}
                     base64 = encode_base64(str); printf("\nYOUR STRING: %s\nBASE64 STRING: %s\n", str, base64); break;
-            case 2: printf("\nINSER THE STRING TO DECODE: "); fgets(str64, __MAX_64_LEN__, stdin); 
+            case 2: printf("\nINSER THE STRING TO DECODE: "); fgets(tmp64, __MAX_64_LEN__, stdin); for(i = 0; i < str_len(tmp64) - 1; i++){str64[i] = tmp64[i];}
                     decoded = decode_base64(str64); printf("\nBASE64 ENCODED STRING: %s\nDECODED STRING: %s\n", str64, decoded); break;
             default: printf("WRONG CHOISE"); break;
         }
