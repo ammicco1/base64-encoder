a23956714ca0091f9c376c2d52882f79a1a591f6
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-01-10 13:35:26 +0100

    file

--- base64_main.c
+++ base64_main.c
@@ -4,14 +4,16 @@
 
 int main(){
     char *base64, *decoded;
-    char str[__MAX_LEN__], str64[__MAX_64_LEN__];
+    char str[__MAX_LEN__], str64[__MAX_64_LEN__], path[50];
     int choise;
+    FILE *file = fopen("test.txt", "r"); 
 
     while(1){
         printf("\n---------------------------------------------\n");
         printf("\nWHAT DO YOU WANT TO DO?\n");
         printf("\t- 1:  ENCODE A STRING IN BASE64\n");
         printf("\t- 2:  DECODE A BASE64 STRING\n");
+        printf("\t- 3:  DECODE A FILE\n");
         printf("\n---------------------------------------------\n");
 
         scanf(" %d", &choise); fflush(stdin);
@@ -21,6 +23,7 @@ int main(){
                     base64 = encode_base64(str); printf("\nYOUR STRING: %s\nBASE64 STRING: %s\n", str, base64); break;
             case 2: printf("\nINSER THE STRING TO DECODE: "); scanf("%s", str64);
                     decoded = decode_base64(str64); printf("\nBASE64 ENCODED STRING: %s\nDECODED STRING: %s\n", str64, decoded); break;
+            case 3: while(!feof(file)){fscanf(file, "%s", path); printf("%s\n", path);} break;
             default: printf("WRONG CHOISE"); break;
         }
     } 
