c73e131d607b52e74d7d17171f99518d4fac2287
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-01-18 11:33:23 +0100

    re

--- base64_main.c
+++ base64_main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define __MAX_PATH__ 60
+
 int main(){
     char *base64, *decoded;
-    char str[__MAX_LEN__], str64[__MAX_64_LEN__], path[60];
+    char str[__MAX_LEN__], str64[__MAX_64_LEN__], path[__MAX_PATH__];
     int choise;
     FILE *file; 
 
@@ -19,13 +21,13 @@ int main(){
         scanf(" %d", &choise); fflush(stdin);
 
         switch(choise){
-            case 1: printf("\nINSERT THE STRING TO ENCODE: "); fgets(str, __MAX_LEN__, stdin); /*scanf("%[^\n]", str);*/
+            case 1: printf("\nINSERT THE STRING TO ENCODE: "); scanf(" %[^\n]", str);
                     base64 = encode_base64(str); printf("\nYOUR STRING: %s\nBASE64 STRING: %s\n", str, base64); break;
-            case 2: printf("\nINSER THE STRING TO DECODE: "); scanf("%s", str64);
+            case 2: printf("\nINSER THE STRING TO DECODE: "); scanf(" %s", str64);
                     decoded = decode_base64(str64); printf("\nBASE64 ENCODED STRING: %s\nDECODED STRING: %s\n", str64, decoded); break;
-            case 3: printf("\nINSERT THE PATH OF THE FILE: "); scanf("%[^\n]", path); file = fopen(path, "r");
+            case 3: printf("\nINSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); file = fopen(path, "r");
                     while(!feof(file)){
-                        fscanf(file, "%s", str64); 
+                        fscanf(file, " %s", str64);
                         decoded = decode_base64(str64);
                         printf("%s\n", decoded);
                     } break;
