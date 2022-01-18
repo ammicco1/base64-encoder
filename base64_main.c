0ade698d93ccdc12feb169333ad6b5834c94a984
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-01-18 11:44:04 +0100

    file

--- base64_main.c
+++ base64_main.c
@@ -8,14 +8,15 @@ int main(){
     char *base64, *decoded;
     char str[__MAX_LEN__], str64[__MAX_64_LEN__], path[__MAX_PATH__];
     int choise;
-    FILE *file; 
+    FILE *file, *wfile; 
 
     while(1){
         printf("\n---------------------------------------------\n");
         printf("\nWHAT DO YOU WANT TO DO?\n");
         printf("\t- 1:  ENCODE A STRING IN BASE64\n");
-        printf("\t- 2:  DECODE A BASE64 STRING\n");
-        printf("\t- 3:  DECODE A FILE\n");
+        printf("\t- 2:  ENCODE A STRING INTO A FILE\n");
+        printf("\t- 3:  DECODE A BASE64 STRING\n");
+        printf("\t- 4:  DECODE A FILE\n");
         printf("\n---------------------------------------------\n");
 
         scanf(" %d", &choise); fflush(stdin);
@@ -23,14 +24,17 @@ int main(){
         switch(choise){
             case 1: printf("\nINSERT THE STRING TO ENCODE: "); scanf(" %[^\n]", str);
                     base64 = encode_base64(str); printf("\nYOUR STRING: %s\nBASE64 STRING: %s\n", str, base64); break;
-            case 2: printf("\nINSER THE STRING TO DECODE: "); scanf(" %s", str64);
+            case 2: printf("\nENCODE INTO A FILE, INSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); wfile = fopen(path, "w");
+                    printf("\nINSERT THE STRING TO ENCODE: "); scanf(" %[^\n]", str);
+                    base64 = encode_base64(str); fprintf(wfile, "%s", base64); fclose(wfile); break;
+            case 3: printf("\nINSER THE STRING TO DECODE: "); scanf(" %s", str64);
                     decoded = decode_base64(str64); printf("\nBASE64 ENCODED STRING: %s\nDECODED STRING: %s\n", str64, decoded); break;
-            case 3: printf("\nINSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); file = fopen(path, "r");
+            case 4: printf("\nINSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); file = fopen(path, "r");
                     while(!feof(file)){
                         fscanf(file, " %s", str64);
                         decoded = decode_base64(str64);
                         printf("%s\n", decoded);
-                    } break;
+                    } fclose(file); break;
             default: printf("WRONG CHOISE"); break;
         }
     } 
