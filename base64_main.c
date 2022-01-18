3a7dfca6bec0d90159c35f4aa2d4e1560b1a3e23
ammicco1 <84914236+ammicco@users.noreply.github.com>
2022-01-18 12:07:01 +0100

    ord

--- base64_main.c
+++ base64_main.c
@@ -14,8 +14,8 @@ int main(){
         printf("\n---------------------------------------------\n");
         printf("\nWHAT DO YOU WANT TO DO?\n");
         printf("\t- 1:  ENCODE A STRING IN BASE64\n");
-        printf("\t- 2:  ENCODE A STRING INTO A FILE\n");
-        printf("\t- 3:  DECODE A BASE64 STRING\n");
+        printf("\t- 2:  DECODE A BASE64 STRING\n\n");
+        printf("\t- 3:  ENCODE A STRING INTO A FILE\n");
         printf("\t- 4:  DECODE A FILE\n\n");
         printf("\t- 5:  ENCODE A FILE INTO A FILE\n");
         printf("\t- 6:  DECODE A FILE INTO A FILE\n");
@@ -26,10 +26,10 @@ int main(){
         switch(choise){
             case 1: printf("\nINSERT THE STRING TO ENCODE: "); scanf(" %[^\n]", str);
                     base64 = encode_base64(str); printf("\nYOUR STRING: %s\nBASE64 STRING: %s\n", str, base64); break;
-            case 2: printf("\nENCODE INTO A FILE, INSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); wfile = fopen(path, "w");
+            case 3: printf("\nENCODE INTO A FILE, INSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); wfile = fopen(path, "w");
                     printf("\nINSERT THE STRING TO ENCODE: "); scanf(" %[^\n]", str);
                     base64 = encode_base64(str); fprintf(wfile, "%s", base64); fclose(wfile); break;
-            case 3: printf("\nINSER THE STRING TO DECODE: "); scanf(" %s", str64);
+            case 2: printf("\nINSER THE STRING TO DECODE: "); scanf(" %s", str64);
                     decoded = decode_base64(str64); printf("\nBASE64 ENCODED STRING: %s\nDECODED STRING: %s\n", str64, decoded); break;
             case 4: printf("\nINSERT THE PATH OF THE FILE: "); scanf(" %[^\n]", path); file = fopen(path, "r");
                     while(!feof(file)){
